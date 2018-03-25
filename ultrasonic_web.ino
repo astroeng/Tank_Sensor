0ab66fee6a73994bf43fbd576fe9e9a20c6989ae
Derek <dschacht@gmail.com>
2018-03-25 18:48:28 -0400

    Updated for the 8266 Thing vice the Thing Dev, increased the sample interval, added median sampling as a comparison to the average sampling.

--- ultrasonic_web.ino
+++ ultrasonic_web.ino
@@ -3,10 +3,28 @@
 #include <ESP8266HTTPClient.h>
 #include <limits.h>
 
+#define SAMPLE_SIZE 16
+#define SENSOR_INTERVAL 30000
+
+#define TRIG 12
+#define PULSE 13
+
+typedef struct
+{
+  unsigned long data;
+  int age;
+} SampleData_Type;
+
+SampleData_Type samples[SAMPLE_SIZE];
+
 ESP8266WebServer server(80);
 
 const char WiFiSSID[] = "sensors";
 const char WiFiPSK[] = "sensors123";
+
+unsigned long sampleTime = 0;
+double distance = 0.0;
+double distanceMedian = 0.0;
 
 // While this (holding the html & javascript text in a string) "works" 
 // it would be much better to get an SD card shield and serve this type 
@@ -15,34 +33,13 @@ const char WiFiPSK[] = "sensors123";
 // seems less than wise. So here it is ... please just scroll to past
 // the web code.
 
-String webpage = 
+String webpage =
 "<!DOCTYPE html> \n\
  <html> \n\
    <head> \n\
      <meta http-equiv=refresh content=300> \n\
      <script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js></script> \n\
      <script src=https://www.google.com/jsapi></script> \n\
-     <script> \n\
-       function drawCharts() { \n\
-          var jsonData = $.ajax({url: 'data.json', dataType: 'jsonp'}).done(function (results) { \n\
-             var dataHumidity = google.visualization.arrayToDataTable([['Sensor', 'Value'], ['Humidity', parseFloat(results[0].h_rp)]]); \n\
-             var dataTemperature = google.visualization.arrayToDataTable([['Sensor', 'Value'], ['Temperature', parseFloat(results[0].t_df)]]); \n\
-             var dataPressure = google.visualization.arrayToDataTable([['Sensor', 'Value'], ['Pressure', parseFloat(results[0].p_in)]]); \n\
-             var optionsHumidity = {width: 800, height: 250, min: 0, max: 100, greenFrom: 40, greedTo: 60, redColor: '#66ff66', redFrom: 35, redTo: 65, minorTicks: 5}; \n\
-             var optionsPressure = {width: 800, height: 250, min: 28, max: 31, greenFrom: 29.25, greedTo: 29.75, minorTicks: 5}; \n\
-             var optionsTemperature = {width: 800, height: 250, min: 0, max: 100, redColor: '#33adff', redFrom: 0, redTo: 32, yellowColor: '#33ffff', yellowFrom: 32, yellowTo: 35, minorTicks: 5}; \n\
-             var chartHumidity = new google.visualization.Gauge($('#gaugeHumidity').get(0)); \n\
-             chartHumidity.draw(dataHumidity, optionsHumidity); \n\
-             var chartTemperature = new google.visualization.Gauge($('#gaugeTemperature').get(0)); \n\
-             chartTemperature.draw(dataTemperature, optionsTemperature); \n\
-             var chartPressure = new google.visualization.Gauge($('#gaugePressure').get(0)); \n\
-             chartPressure.draw(dataPressure, optionsPressure); \n\
-             document.getElementById('time').innerHTML = parseFloat(results[0].time); \n\
-        }); \n\
-      } \n\
-      google.load('visualization', '1', {packages: ['corechart','gauge']}); \n\
-      google.setOnLoadCallback(drawCharts); \n\
-     </script></head> \n\
      <body><table width=80% align=center> \n\
        <tr> \n\
          <td><div id='time' style='width: 40%;'></div></td> \n\
@@ -52,27 +49,26 @@ String webpage =
          <td><p></td> \n\
          <td>Sample Time</td> \n\
          <td>Valid</td> \n\
-         <td>Distance</td> \n\
+         <td>Distance (avg)</td> \n\
+         <td>Distance (median)</td> \n\
       </tr><tr> \n\
          <td>HCSR04</td> \n\
-         <td><div id='timeS0' style='width: 33%;'></div></td> \n\
-         <td><div id='validS0' style='width: 33%;'></div></td> \n\
-         <td><div id='distanceS0' style='width: 34%;'></div></td> \n\
+         <td><div id='timeS0' style='width: 25%;'></div></td> \n\
+         <td><div id='validS0' style='width: 25%;'></div></td> \n\
+         <td><div id='distanceS0' style='width: 25%;'></div></td> \n\
+         <td><div id='distanceMS0' style='width: 25%;'></div></td> \n\
       </tr></table><script> \n\
        function loadSensorData() { \n\
-         var jsonData = $.ajax({url: 'sensor.json', dataType: 'jsonp'}).done(function (results) { \n\
+         var jsonData = $.ajax({url: 'data.json', dataType: 'jsonp'}).done(function (results) { \n\
            document.getElementById('timeS0').innerHTML = parseFloat(results[0].time); \n\
            document.getElementById('validS0').innerHTML = parseFloat(results[0].valid); \n\
            document.getElementById('distanceS0').innerHTML = parseFloat(results[0].d_in); \n\
+           document.getElementById('distanceMS0').innerHTML = parseFloat(results[0].dm_in); \n\
          }); \n\
        } \n\
        loadSensorData(); \n\
      </script> \n\
      </body></html>\n";
-
-
-double distance = 0.0;
-unsigned long sampleTime = 0;
 
 void handleRoot() 
 {
@@ -85,15 +81,16 @@ void dataJSON()
 
   String message = "typeof " + value + " === 'function' && " + value + 
                    "([{ time:" + String(sampleTime) + "," + 
-                       "d_in:" + String(distance) + " }])\n";
+                       "d_in:" + String(distance) + "," +
+                       "dm_in:" + String(distanceMedian) + " }])\n";
 
   server.send(200, "text/plain", message);
 }
 
-String JSON_SensorValues()
+String JSON_SensorValues(SampleData_Type data)
 {
-  String element = "{ time:" + String(sampleTime) + "," + 
-                       "d_in:" + String(distance) + " }";
+  String element = "{ age:" + String(data.age) + "," + 
+                       "d_in:" + String(data.data) + " }\n  ";
   return element;
 }
 
@@ -102,7 +99,22 @@ void sensorJSON()
   String value = server.arg("callback");
 
   String message = "typeof " + value + " === 'function' && " + value + 
-                   "([" + JSON_SensorValues() + "]);\n";
+                   "([" + JSON_SensorValues(samples[0]) + "," + 
+                          JSON_SensorValues(samples[1]) + "," + 
+                          JSON_SensorValues(samples[2]) + "," + 
+                          JSON_SensorValues(samples[3]) + "," + 
+                          JSON_SensorValues(samples[4]) + "," + 
+                          JSON_SensorValues(samples[5]) + "," + 
+                          JSON_SensorValues(samples[6]) + "," + 
+                          JSON_SensorValues(samples[7]) + "," + 
+                          JSON_SensorValues(samples[8]) + "," + 
+                          JSON_SensorValues(samples[9]) + "," + 
+                          JSON_SensorValues(samples[10]) + "," + 
+                          JSON_SensorValues(samples[11]) + "," + 
+                          JSON_SensorValues(samples[12]) + "," + 
+                          JSON_SensorValues(samples[13]) + "," + 
+                          JSON_SensorValues(samples[14]) + "," + 
+                          JSON_SensorValues(samples[15]) + "]);\n";
 
   server.send(200, "text/plain", message);
 }
@@ -160,12 +172,6 @@ void setup_serial()
 }
 
 
-#define SENSOR_INTERVAL 1000
-
-#define POWER 0
-#define TRIG 12
-#define PULSE 15
-
 double pulseLength = 0;
 
 unsigned long startMeasurementTime = SENSOR_INTERVAL;
@@ -179,7 +185,7 @@ void pulseISR()
   // Note: micros will not increment during the ISR.
 
   // If pulseStart is less than pulseEnd it means that
-  // this is the start of a new pulse. Otherwise this
+  // this is the start of a new pulse. Otherwise, this
   // is the end of a pulse.
 
   if (pulseStart < pulseEnd)
@@ -203,12 +209,20 @@ void pulseISR()
   
 }
 
+
+
 void setup() 
 {
   setup_serial();
 
   setupWifi();
   setupServer();
+
+  for (int i = 0; i < SAMPLE_SIZE; i++)
+  {
+    samples[i].data = 0;
+    samples[i].age = 0;
+  }
 
   pinMode(TRIG, OUTPUT);
   pinMode(PULSE, INPUT);
@@ -220,6 +234,51 @@ void setup()
   // NOTE: on the ESP8266 all I/O pins can be interrupt pins.
   attachInterrupt(digitalPinToInterrupt(PULSE), pulseISR, CHANGE);
 
+}
+
+void addElement(unsigned long value)
+{
+  bool added = false;
+   
+  for (int i = 0; i < SAMPLE_SIZE; i++)
+  {
+    if (samples[i].age <= 0 && !added)
+    {
+      samples[i].data = value;
+      samples[i].age  = SAMPLE_SIZE-1;
+      added = true;
+    }
+    else
+    {
+      samples[i].age--;
+    }
+  }
+}
+
+double getDistance()
+{
+  unsigned long temp[SAMPLE_SIZE];
+   
+  for (int i = 0; i < SAMPLE_SIZE; i++)
+  {
+    temp[i] = samples[i].data;
+  }
+
+  for (int i = 0; i < SAMPLE_SIZE-1; i++)
+  {
+    for (int j = i+1; j < SAMPLE_SIZE; j++)
+    {
+      if (temp[i] > temp[j])
+      {
+        unsigned long temp2 = temp[i];
+        temp[i] = temp[j];
+        temp[j] = temp2;  
+      }  
+    }  
+  }
+  
+  return (temp[5] + temp[6] + temp[7] + temp[8] + temp[9] + temp[10]) / 6.0;
+  
 }
 
 void loop() 
@@ -241,10 +300,12 @@ void loop()
   if (pulseHappened)
   {
     sampleTime = millis();
-    pulseLength = (pulseLength * 0.8) + ((pulseEnd - pulseStart) * 0.2);
+    addElement(pulseEnd - pulseStart);
+    pulseLength = (pulseLength * 0.9) + ((pulseEnd - pulseStart) * 0.1);
     pulseHappened = false;
 
-    distance = (pulseLength )  /  148.0;
+    distance = (pulseLength)  /  148.0;
+    distanceMedian = getDistance()   /  148.0;
   }
 
 }
